b2dab6ddeec904fe8c6292030ccd2eb2b3d4b7ce
shreyarkay <99129405+shreyarkay@users.noreply.github.com>
2024-06-06 22:45:43 +0530

    Add files via upload

--- Task-3/Student.java
+++ Task-3/Student.java
@@ -1,1 +1,73 @@
+import java.util.Objects;
 
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String grade;
+    private String address;
+
+    public Student(int id, String name, int age, String grade, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.address = address;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age + ", Grade: " + grade + ", Address: " + address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

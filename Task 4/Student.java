3bcb085ff138291b7d6637f8d7f42b0f677d79a9
shreyarkay <99129405+shreyarkay@users.noreply.github.com>
2024-06-15 22:01:25 +0530

    Create Student.java

---
+++ Task 4/Student.java
@@ -0,0 +1,58 @@
+package studentmgmt;
+
+
+public class Student {
+    private Long id;
+    private String name;
+    private int age;
+    private String grade;
+    private String address;
+
+    public Student() {}
+
+    public Student(Long id, String name, int age, String grade, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.address = address;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public String getGrade() { return grade; }
+    public void setGrade(String grade) { this.grade = grade; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        return id != null ? id.equals(student.id) : student.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}

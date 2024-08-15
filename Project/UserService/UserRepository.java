94d14f7a2c7bd8c7e1d416a6826ce938e9b0421e
shreyarkay <99129405+shreyarkay@users.noreply.github.com>
2024-08-15 15:32:07 +0530

    Delete Project/UserService directory

--- Project/UserService/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.project.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
